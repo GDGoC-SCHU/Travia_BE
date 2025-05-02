1028540b1ffccea43bbbf4f0e8180e1dd39375b7
seojin <145626665+Seojin2022@users.noreply.github.com>
2025-05-02 22:22:40 +0900

    api update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import survey, recommendation
+from app.api.v1.endpoints import survey
 from app.db.init_db import init_db
 
 app = FastAPI(title="Travia API", version="1.0")
@@ -7,4 +7,3 @@ init_db()
 
 # 라우터 연결
 app.include_router(survey.router, prefix="/api/v1")
-app.include_router(recommendation.router, prefix="/api/v1")
